fa19846449c2ab6a00e8eb595f1b84c2d9d4d386
Sara Caramaschi <caramaschisara@gmail.com>
2021-12-02 19:53:10 +0100

    chiamata oggi

--- stage2_3.m
+++ stage2_3.m
@@ -1,15 +1,21 @@
 function result = stage2_3(signal, fs)
+
     fn=fs/2; %Nyquist frequenc
     fc_low=15/fn; %normalized inferior cut off frequency
     fc_high=0.01/fn; %normalized superior cut off frequency
     N=2048; %arbitrary
     
-    pad=20;
+    
+    pad=20; % valore dei due sample piu esterni
     signal_to_filter = [ones(pad,1)*signal(1); signal(:); ones(pad,1)*signal(end)];
     
-    order = 5; 
-    [b,a] = butter(order,[fc_low fc_high],'bandpass');
+    
+    order = 5; % specificato nel paper
+    [b,a] = butter(order,[fc_low fc_high],'bandpass'); 
+    
     freqz(b,a,N)%,fs)
-    res = filtfilt(b,a,signal_to_filter);%zero phase filtering
+    % Applichiamo filtro. Attenzione TUTTI NAN!!!!! 
+    res = filtfilt(b,a,signal_to_filter); % zero phase filtering
     result=res(pad+1:size(res,1)-pad);
+    
 end
